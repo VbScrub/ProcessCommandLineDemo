03a6963d30ef045fc78adae3615991c7491fec33
VbScrub <53273102+VbScrub@users.noreply.github.com>
2020-05-04 02:11:31 +0100

    Add files via upload

---
+++ ProcessCmdLineDemo/Program.vb
@@ -0,0 +1,9 @@
+﻿Module Program
+
+    Sub Main()
+        'Example just showing the command line used to launch the current process
+        Console.WriteLine(WindowsApi.GetCommandLine(Process.GetCurrentProcess))
+        Console.ReadLine()
+    End Sub
+
+End Module
