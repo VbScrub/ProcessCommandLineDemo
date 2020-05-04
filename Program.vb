b3d0a14930197d3653f8e6fcdc97e6128b14a2c1
VbScrub <53273102+VbScrub@users.noreply.github.com>
2020-05-04 02:10:51 +0100

    Delete Program.vb

--- Program.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Program
-
-    Sub Main()
-        'Example just showing the command line used to launch the current process
-        Console.WriteLine(WindowsApi.GetCommandLine(Process.GetCurrentProcess))
-        Console.ReadLine()
-    End Sub
-
-End Module
